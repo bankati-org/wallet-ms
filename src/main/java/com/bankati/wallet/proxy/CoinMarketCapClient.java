57e59a79063cd42c06e8f1a9d5c37282893623f8
Minaoui Mohamed <minaouimh@gmail.com>
2025-01-08 22:28:58 +0100

    test

--- src/main/java/com/bankati/wallet/proxy/CoinMarketCapClient.java
+++ src/main/java/com/bankati/wallet/proxy/CoinMarketCapClient.java
@@ -1,6 +1,7 @@
 package com.bankati.wallet.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @FeignClient(name = "coinMarketCapClient", url = "https://pro-api.coinmarketcap.com/v1")
+@Service
 public interface CoinMarketCapClient {
 
     @GetMapping("/cryptocurrency/quotes/latest")
--- src/main/java/com/bankati/wallet/proxy/CurrencyExchangeClient.java
+++ src/main/java/com/bankati/wallet/proxy/CurrencyExchangeClient.java
@@ -1,6 +1,7 @@
 package com.bankati.wallet.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @FeignClient(name = "currency-exchange", url = "https://v6.exchangerate-api.com/v6")
+@Service
+
 public interface CurrencyExchangeClient {
 
     @GetMapping("/${api.exchangerates.access_key}/latest/{base_code}")
